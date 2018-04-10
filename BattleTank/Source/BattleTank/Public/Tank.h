147e20e66c03cd46025d8af0a42ad694861f7bd7
TalimeQ <b.kuchta996@gmail.com>
2018-04-10 18:58:44 +0200

    Rozdzielenie czesci odpowiedzialnej za aming do komponentu, referencja do lufy czolgu

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -15,10 +16,13 @@ public:
 	// Sets default values for this pawn's properties
 	ATank();
 	void AimAt(FVector HitLocation);
+	UFUNCTION(BlueprintCallable, Category = "setup")
+	void SetBarrelComponent(UStaticMeshComponent* BarrelToSet);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 public:	
 	// Called every frame
